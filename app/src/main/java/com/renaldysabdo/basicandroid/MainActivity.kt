d551941d80845d84ee3c8865960e3838938388ca
renaldy <renalraion@gmail.com>
2021-01-04 07:06:41 +0700

    [refactor] refactor shared preferences

--- app/src/main/java/com/renaldysabdo/basicandroid/MainActivity.kt
+++ app/src/main/java/com/renaldysabdo/basicandroid/MainActivity.kt
@@ -8,7 +8,6 @@ import com.renaldysabdo.basicandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var resource : ResourceManagement
     private lateinit var binding : ActivityMainBinding
 
     @SuppressLint("SetTextI18n")
@@ -17,23 +16,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        resource = ResourceManagement(this@MainActivity)
 
         binding.btnSave.setOnClickListener {
             //save value
             val username = binding.etUsername.text.toString()
             val age = binding.etAge.text.toString()
 
-            resource.setUserName(username)
-            resource.setUserAge(age)
-
-            //get value
-            resource.liveSharedPreferences.listenMultiple(listOf(USER_NAME_KEY, USER_AGE_KEY), "").observe(this, {
-                with(binding.textResult){
-                    visibility = View.VISIBLE
-                    text = "hallo ${it[USER_NAME_KEY]}, your age : ${it[USER_AGE_KEY]}"
-                }
-            })
         }
 
     }
