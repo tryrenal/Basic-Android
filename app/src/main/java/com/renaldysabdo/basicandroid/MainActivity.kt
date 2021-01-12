82f98ec1feb79c092c105d380acd98f1cbf3431f
renaldy <renalraion@gmail.com>
2021-01-12 21:40:14 +0700

    [add] adding logic to impl mode night

--- app/src/main/java/com/renaldysabdo/basicandroid/MainActivity.kt
+++ app/src/main/java/com/renaldysabdo/basicandroid/MainActivity.kt
@@ -2,10 +2,48 @@ package com.renaldysabdo.basicandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
+import com.renaldysabdo.basicandroid.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //set theme to default setting device
+        binding.btnDefault.setOnClickListener(this)
+
+        //set theme to night
+        binding.btnNight.setOnClickListener(this)
+
+        //set theme to light
+        binding.btnLight.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btnDefault -> {
+               val theme = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                implThemeChoosen(theme)
+            }
+            R.id.btnLight -> {
+               val theme = AppCompatDelegate.MODE_NIGHT_NO
+                implThemeChoosen(theme)
+            }
+            R.id.btnNight -> {
+                val theme = AppCompatDelegate.MODE_NIGHT_YES
+                implThemeChoosen(theme)
+            }
+        }
+    }
+
+    private fun implThemeChoosen(theme: Int) {
+        //setting implementasi theme choosen
+        AppCompatDelegate.setDefaultNightMode(theme)
     }
 }
