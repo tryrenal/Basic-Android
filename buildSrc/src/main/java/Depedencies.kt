15b8d7845e9b0a1a973c552b0a31f5005f9a8eab
renaldy <renalraion@gmail.com>
2021-02-18 14:02:27 +0700

    [add] adding build gradle management

---
+++ buildSrc/src/main/java/Depedencies.kt
@@ -0,0 +1,19 @@
+object Versions {
+    val coreKtx = "1.3.2"
+    val appCompat = "1.2.0"
+    val material = "1.2.1"
+    val constraintlayout = "2.0.4"
+    val junit = "4.13.1"
+    val extJunit = "1.1.2"
+    val espressoCore = "3.3.0"
+}
+
+object Libraries {
+    val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+    val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    val material = "com.google.android.material:material:${Versions.material}"
+    val constraintlayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintlayout}"
+    val junit = "junit:junit:${Versions.junit}"
+    val extJunit = "androidx.test.ext:junit:${Versions.extJunit}"
+    val espressoCore = "androidx.test.espresso:espresso-core:${Versions.espressoCore}"
+}
