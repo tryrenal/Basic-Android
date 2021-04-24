c9a81b7157139d632a2af8fa4bc035ee345c65f8
renaldy <renalraion@gmail.com>
2021-04-24 20:52:32 +0700

    add animation zoom in

--- app/src/main/java/com/renaldysabdo/basicandroid/MainActivity.kt
+++ app/src/main/java/com/renaldysabdo/basicandroid/MainActivity.kt
@@ -2,10 +2,22 @@ package com.renaldysabdo.basicandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageLogo = findViewById<ImageView>(R.id.imageLogo)
+
+        val btnZoomIn = findViewById<Button>(R.id.btnZoomIn)
+        btnZoomIn.setOnClickListener {
+            imageLogo.startAnimation(AnimationUtils.loadAnimation(
+                    this, R.anim.zoom_in
+            ))
+        }
     }
 }
