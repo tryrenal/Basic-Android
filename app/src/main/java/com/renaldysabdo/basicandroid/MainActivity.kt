7a9e41e20f8f1a23f46acb549937db64fb9bc50c
renaldy <renalraion@gmail.com>
2021-05-01 00:44:07 +0700

    setup binding activity with inflater

--- app/src/main/java/com/renaldysabdo/basicandroid/MainActivity.kt
+++ app/src/main/java/com/renaldysabdo/basicandroid/MainActivity.kt
@@ -9,12 +9,12 @@ import com.renaldysabdo.basicandroid.model.Data
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val data = Data("Renaldy", 23)
+        val data = Data("renaldy", 16)
         binding.data = data
     }
 
