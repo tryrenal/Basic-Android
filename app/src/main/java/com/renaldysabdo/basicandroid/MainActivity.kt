ee07b53041121b4260e4a42e9d613a1c9a0de2e2
renaldy <renalraion@gmail.com>
2021-04-30 21:49:31 +0700

    setting databiding basic

--- app/src/main/java/com/renaldysabdo/basicandroid/MainActivity.kt
+++ app/src/main/java/com/renaldysabdo/basicandroid/MainActivity.kt
@@ -2,10 +2,22 @@ package com.renaldysabdo.basicandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.renaldysabdo.basicandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        settingText()
+
+    }
+
+    private fun settingText(){
+        binding.tvGreeting.text = "Renaldy Sabdo Jati"
     }
 }
