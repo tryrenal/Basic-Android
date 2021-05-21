7fc8142b9bdfd48b8e86d6006a435cd9284913e5
renaldy <renalraion@gmail.com>
2021-05-21 09:42:07 +0700

    navigation component v1

--- app/src/main/java/com/renaldysabdo/basicandroid/MainActivity.kt
+++ app/src/main/java/com/renaldysabdo/basicandroid/MainActivity.kt
@@ -2,10 +2,49 @@ package com.renaldysabdo.basicandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.renaldysabdo.basicandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentNavController: LiveData<NavController>? = null
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        if (savedInstanceState == null){
+            setupBottomNavigationBar()
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigationBar()
+    }
+
+    private fun setupBottomNavigationBar(){
+        val bottomNavigationView = binding.bottomNav
+        val navGraphIds = listOf(R.navigation.nav_graph_home, R.navigation.nav_graph_info)
+
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_fragment,
+            intent = intent
+        )
+
+        controller.observe(this, Observer { navController ->
+            setupActionBarWithNavController(navController)
+        })
+        currentNavController = controller
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
     }
 }
