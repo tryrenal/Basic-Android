55bfc952445e03062c16af4ace0855684f4df2d9
renaldy <renalraion@gmail.com>
2021-05-24 09:29:41 +0700

    adding logic for navigation component

--- app/src/main/java/com/renaldysabdo/basicandroid/MainActivity.kt
+++ app/src/main/java/com/renaldysabdo/basicandroid/MainActivity.kt
@@ -2,10 +2,32 @@ package com.renaldysabdo.basicandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViews()
+    }
+
+    private fun setupViews(){
+        val navController = findNavController(R.id.fragNavHost)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavView)
+        bottomNav.setupWithNavController(navController)
+
+        var appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(
+                R.id.homeFragment,
+                R.id.infoFragment
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
